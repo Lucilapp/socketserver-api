177392d354d500c5136190acdcc9482a7414eb88
Android Studio <devandroid@ort>
2024-09-04 08:46:36 -0300

    funciona

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-
+console.log('Server OK')
 io = require('socket.io')(5000, {
     cors:  {
-        origin: ['http://localhost:3000']
+        origin: ['http://localhost:8081/'],
+        methods: ['GET', 'POST'],
     }
 })
 io.on('connection', socket => {
     console.log(socket.id)
     socket.on('messageSend', (socketId, msg, reciever) => {
-        io.to(reciever).emit('recieveMessage', msg, socketId)
+        console.log(socketId, msg, reciever)
+        io.emit('serverConnected')
+        // io.to(reciever).emit('recieveMessage', msg, socketId)
     })
+    
 })
